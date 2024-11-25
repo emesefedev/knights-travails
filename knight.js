78214ede5f954c5d1153e5f9eba711d39c8a8ea8
emesefedev <emesefedev@gmail.com>
2024-11-25 17:09:36 +0100

    refactor: knight class

--- knight.js
+++ knight.js
@@ -29,72 +29,79 @@ const BoardPosition = class {
     }
 }
 
-function getValidKnightBoardPositionsFromPosition(position) {
-    const [x, y] = position
+const Knight = class {
+    constructor() {
+        this.graph = this.buildMovesGraph()
+    } 
+
+    getValidKnightBoardPositionsFromPosition(position) {
+        const [x, y] = position
+        
+        const boardPositions = knightMoves.map((possibleMove) => {
+            const [offsetX, offsetY] = possibleMove
+            return new BoardPosition(x + offsetX, y + offsetY)
+        })
     
-    const boardPositions = knightMoves.map((possibleMove) => {
-        const [offsetX, offsetY] = possibleMove
-        return new BoardPosition(x + offsetX, y + offsetY)
-    })
-
-    const validBoardPositions = boardPositions.filter((boardPosition) => {
-        return boardPosition.isValid()
-    })
-
-    return validBoardPositions
-}
-
-function buildMovesGraph() {
-    const graph = new Map()
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            graph.set(`[${i},${j}]`, getValidKnightBoardPositionsFromPosition([i, j]))
-        }   
+        const validBoardPositions = boardPositions.filter((boardPosition) => {
+            return boardPosition.isValid()
+        })
+    
+        return validBoardPositions
     }
-
-    return graph
-}
-
-function knightTravails(origin, destination) {
-
-    const start = new BoardPosition(...origin)
-    const target = new BoardPosition(...destination)
     
-    const queue = [start]
-
-    while (!includesBoardPosition(queue, target)) {
-        const boardPosition = queue.shift()
-
-        for (const possibleMove of graph.get(boardPosition.getName())) {
-            possibleMove.setPredecessor(boardPosition)
-            queue.push(possibleMove)
+    buildMovesGraph() {
+        const graph = new Map()
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                graph.set(`[${i},${j}]`, this.getValidKnightBoardPositionsFromPosition([i, j]))
+            }   
+        }
+    
+        return graph
+    }
+    
+    knightTravails(origin, destination) {
+    
+        const start = new BoardPosition(...origin)
+        const target = new BoardPosition(...destination)
+        
+        const queue = [start]
+    
+        while (!this.includesBoardPosition(queue, target)) {
+            const boardPosition = queue.shift()
+    
+            for (const possibleMove of this.graph.get(boardPosition.getName())) {
+                possibleMove.setPredecessor(boardPosition)
+                queue.push(possibleMove)
+            }
+        }
+    
+        const path = [queue[this.findBoardPosition(queue, target)]]
+        while (!this.includesBoardPosition(path, start)) {
+            const predecessor = path[0].getPredecessor()
+            path.unshift(predecessor)
+        }
+    
+        console.log(`The shortest path was ${path.length - 1} moves!`)
+        console.log(`The moves were: `)
+        for (const boardPosition of path) {
+            console.log(boardPosition.getName())
         }
     }
-
-    const path = [queue[findBoardPosition(queue, target)]]
-    while (!includesBoardPosition(path, start)) {
-        const predecessor = path[0].getPredecessor()
-        path.unshift(predecessor)
+    
+    includesBoardPosition(array, value) {
+        return array.some((boardPosition) => {
+            return boardPosition.equals(value)
+        })
     }
-
-    console.log(`The shortest path was ${path.length - 1} moves!`)
-    console.log(`The moves were: `)
-    for (const boardPosition of path) {
-        console.log(boardPosition.getName())
+    
+    findBoardPosition(array, value) {
+        return array.findIndex((boardPosition) => {
+            return boardPosition.equals(value)
+        })
     }
 }
 
-function includesBoardPosition(array, value) {
-    return array.some((boardPosition) => {
-        return boardPosition.equals(value)
-    })
-}
 
-function findBoardPosition(array, value) {
-    return array.findIndex((boardPosition) => {
-        return boardPosition.equals(value)
-    })
-}
-
-const graph = buildMovesGraph()
-knightTravails([3, 3], [4, 2])
+const knight = new Knight()
+knight.knightTravails([3, 3], [4, 2])
